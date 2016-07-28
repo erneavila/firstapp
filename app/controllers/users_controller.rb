ca2eed1e88a4e9b8bc8b690505ed0b41fc5f2edb
erneavila <erneavila@hotmail.com>
2016-07-28 16:39:01 -0500

    commit inicial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def index
+    @usuarios = User.all
+  end
+  def show
+    @usuario=User.find(params[:id])
+    render action: "detalle"
+  end
+  def detalle
+    @usuario=User.find(1)
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+  def edit
+    @usuario=User.find(params[:id])
+  end
+  def update
+    @usuario=User.find(params[:id])
+    if @usuario.update_attributes(user_params)
+      redirect_to users_url
+    else
+      render action: 'edit'
+    end
+  end
+
+  def new
+    @usuario = User.new
+  end
+
+  def create
+    @usuario=User.new(user_params)
+    if @usuario.save
+      flash[:success] = "Bienvenido"
+      redirect_to users_url
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nombre, :email,:password, :password_confirmation)
+  end
+end
